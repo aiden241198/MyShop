89357564b4d815393ca51be626aa9e274752c0da
Aiden Smart <aidensmart24@hotmail.co.uk>
2020-06-10 20:30:40 +0100

    added memery cache and product methods

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string ID { set; get; }
+        public string Name { get; set; }
+        public string Description { set; get; }
+
+        [Range(0, 1000)]
+        public decimal Price { set; get; }
+        public string Category { set; get; }
+        public string Image { set; get; }
+
+        public Product()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+    }
+}
