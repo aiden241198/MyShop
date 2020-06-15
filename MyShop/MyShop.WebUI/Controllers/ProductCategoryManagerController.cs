a25cac226e5aa66ef2ca42ad1a6929c67773a31b
Aiden Smart <aidensmart24@hotmail.co.uk>
2020-06-15 16:02:20 +0100

    Product Grouping with drop down menu

---
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,120 @@
+﻿using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        ProductCategoryRepository context;
+
+        public ProductCategoryManagerController()
+        {
+            context = new ProductCategoryRepository();
+        }
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else
+            {
+                context.Insert(productCategory);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string ID)
+        {
+            ProductCategory productCategory = context.Find(ID);
+
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, string ID)
+        {
+            ProductCategory categoryToEdit = context.Find(ID);
+
+            if (categoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCategory);
+                }
+
+                categoryToEdit.Category = productCategory.Category;
+
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string ID)
+        {
+            ProductCategory categoryToDelete = context.Find(ID);
+
+            if (categoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(categoryToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string ID)
+        {
+            ProductCategory categoryToDelete = context.Find(ID);
+
+            if (categoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(ID);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
