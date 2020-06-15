d66e344ce3630e06b0f497a8fd3ca59d1e8ba194
Aiden Smart <aidensmart24@hotmail.co.uk>
2020-06-15 16:33:58 +0100

    added inMemory cache

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -8,10 +8,8 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class Product
+    public class Product : BaseEntity
     {
-
-        public string ID { get; set; }
 
         [StringLength(20)]
         [DisplayName("Product Name")]
@@ -23,9 +21,5 @@ namespace MyShop.Core.Models
         public string Category { get; set; }
         public string Image { get; set; }
 
-        public Product()
-        {
-            this.ID = Guid.NewGuid().ToString();
-        }
     }
 }
